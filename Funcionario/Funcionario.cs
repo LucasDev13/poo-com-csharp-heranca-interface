13f987529a2b58a10b5c64a80fd5d9fed0fc9bab
Lucas Pontes <lucasdev@pop-os.localdomain>
2021-10-20 16:24:44 -0300

    Criando a classe Funcionario.

---
+++ Funcionario/Funcionario.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace poo_heranca_interface.Funcionario
+{
+    public class Funcionario
+    {
+
+        private int _tipo;
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario(int tipo){
+            _tipo = tipo;
+        }
+
+        public double GetBonificacao(){
+            if(_tipo == 0){
+                return Salario;
+            }
+            return Salario * 0.10;
+        }
+        
+    }
+}
